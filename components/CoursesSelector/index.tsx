6b4c9951b4f7a221657e2b7b9ab9b8cb702fcfb5
Gerison Kimathi <gerkim62@gmail.com>
2024-01-14 23:47:08 +0300

    Refactor Next button in CoursesSelector component

--- components/CoursesSelector/index.tsx
+++ components/CoursesSelector/index.tsx
@@ -81,7 +81,18 @@ const CoursesSelector = ({ options }: Props) => {
           onClick={handleNext}
           className="bg-emerald-500 text-white flex items-center justify-center px-4 py-2 rounded-md w-full hover:bg-emerald-600 disabled:opacity-50 disabled:cursor-not-allowed"
         >
-          Next <FaArrowRight className="ml-2" />
+          {isPending ? (
+            <>
+              <span className="animate-spin rounded-full h-4 w-4 border-b-2 border-white mr-2"></span>{" "}
+              Saving... 
+              <span className="sr-only">Processing Selection...</span>
+            </>
+          ) : (
+            <>
+              {" "}
+              Next <FaArrowRight className="ml-2" />
+            </>
+          )}
         </button>
       </div>
     </div>
