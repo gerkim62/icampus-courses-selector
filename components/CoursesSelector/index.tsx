7ea1d371684d036946ec7d626df9a7b2b2a110d5
Gerison Kimathi <gerkim62@gmail.com>
2024-01-14 23:12:28 +0300

    Add disabled state to Next button in CoursesSelector component

--- components/CoursesSelector/index.tsx
+++ components/CoursesSelector/index.tsx
@@ -77,8 +77,9 @@ const CoursesSelector = ({ options }: Props) => {
       {/* Next button */}
       <div className="w-full mt-4">
         <button
+          disabled={isPending}
           onClick={handleNext}
-          className="bg-emerald-500 text-white flex items-center justify-center px-4 py-2 rounded-md w-full hover:bg-emerald-600"
+          className="bg-emerald-500 text-white flex items-center justify-center px-4 py-2 rounded-md w-full hover:bg-emerald-600 disabled:opacity-50 disabled:cursor-not-allowed"
         >
           Next <FaArrowRight className="ml-2" />
         </button>
